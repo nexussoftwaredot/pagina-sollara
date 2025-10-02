4c64ba132b8f56e759d44af21875333eb2f3855f
Dan Nunes <cursejon8@gmail.com>
2025-10-02 14:15:53 -0300

    Update CasaUmSection.tsx

--- src/components/CasaUmSection.tsx
+++ src/components/CasaUmSection.tsx
@@ -32,7 +32,7 @@ const CasaUmSection = () => {
 
         {/* Texto CASAS EXCLUSIVAS PARA PCD com ícone */}
         <div className="flex items-center justify-center gap-2 text-book-text">
-          <span className="text-xl md:text-2xl font-bold">CASAS EXCLUSIVAS PARA PCD</span>
+          <span className="text-xl md:text-2xl font-bold">CASAS EXCLUSIVAS PARA PESSOAS COM DEFICIÊNCIA PCD</span>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-6 h-6 md:w-7 md:h-7 text-blue-600 fill-current">
             <path d="M192 96c26.5 0 48-21.5 48-48s-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48zm-8 384V352h16V480c0 17.7 14.3 32 32 32s32-14.3 32-32V192h-64l0 0H97.6c-19.6 0-35.1-17.9-31.3-37.1l18.1-92c3.5-17.8 19.1-30.9 37.3-30.9H304c17.7 0 32-14.3 32-32s-14.3-32-32-32H121.7C74.9 0 33.5 34.5 24.3 81L6.2 173C-10.6 233 35.5 288 97.6 288H160V352h16V224h64V384H128c-17.7 0-32 14.3-32 32s14.3 32 32 32H240z"/>
           </svg>
